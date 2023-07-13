cf3cdac4ba8775152f73167d80f3d1aa7c55acd5
itc-n23004 <n23004@std.it-college.ac.jp>
2023-07-13 07:33:28 +0000

    Q3_6

--- ch2/Q3/Q3_6_1.py
+++ ch2/Q3/Q3_6_1.py
@@ -1,11 +1,11 @@
 import random
 
-a = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+a = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 sample4 = random.sample(a, k=4)
-n4 = "".join(sample4)
+num4 = "".join(sample4)
 while True:
     i = input()
-    if i == n4:
+    if i == num4:
         print("ok")
         break
     else:
