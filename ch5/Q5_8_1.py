9c66f6523add1277eb6c9fa447fe31336fcbf077
itc-n23004 <n23004@std.it-college.ac.jp>
2023-08-16 07:06:00 +0000

    比較

--- ch5/Q5_8_1.py
+++ ch5/Q5_8_1.py
@@ -1,2 +1,19 @@
-a = {x: x * 3 for x in range(11)}
-print(a)
+country = {
+    "Iceland": {"code": "354", "capital": "Reykjavik"},
+    "Ireland": {"code": "353", "capital": "Dublin"},
+    "Azerbaidjan": {"code": "994", "capital": "Baku"},
+}
+
+
+def getstr_keyval(x):
+    if not isinstance(x, dict):
+        return x
+
+    my_str = ""
+    for key, val in x.items():
+        my_str += " " + str(key) + " " + getstr_keyval(val)
+    return my_str
+
+
+for key1, val1 in country.items():
+    print(key1, getstr_keyval(val1))
